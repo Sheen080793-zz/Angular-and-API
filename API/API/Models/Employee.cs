9541f3271117855bcd3f57b848431311fd5554fa
Sheen_LIG <sheenmichael_aking@cody.inc>
2022-02-08 23:22:07 +0800

    Angular and API

---
+++ API/API/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+        public string Position { get; set; }
+    }
+}
